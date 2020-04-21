267e03c1d53de333a9d0844c71e5c9aca72db1d3
magician3124 <crashingballoon@gmail.com>
2020-04-21 23:00:31 +0800

    add to js

--- header.js
+++ header.js
@@ -11,4 +11,46 @@ document.write(`
             <li><a href="#"><i class="fas fa-map-pin"></i></a></li> -->
         </ul>
     </div>
+    <div class="main_content">
+        <div class="hero-image">
+          <div class="hero-text">
+            <h1>Welcome to LoopHole</h1>
+            <p>By Finn</p>
+          </div>
+        </div>
+        <div class="droplist">
+            <div class="dropdown">
+              <button class="dropbtn">Ｎetwork Engineering</button>
+              <div class="dropdown-content">
+                <a href="http://loophole.nctu.me/subwebsite/network/osimodel.html">OSI Model</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div>
+            <div class="dropdown">
+              <button class="dropbtn">POST</button>
+              <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div>
+            <div class="dropdown">
+              <button class="dropbtn">POST</button>
+              <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div>
+            <div class="dropdown">
+              <button class="dropbtn">POST</button>
+              <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div>
+        </div>
+    </div>
 `);
