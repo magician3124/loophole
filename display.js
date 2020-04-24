39c4e57fe336f7935f7847b3cc49cb3f248fc720
magician3124 <crashingballoon@gmail.com>
2020-04-25 00:09:26 +0800

    -> js

---
+++ display.js
@@ -0,0 +1,17 @@
+function appearphysic() {
+var a = document.getElementById("physic");
+    if (a.style.display === "none") {
+        a.style.display = "block";
+    } else {
+        a.style.display = "none";
+    }
+}
+
+function appeardata() {
+var b = document.getElementById("data");
+    if (b.style.display === "none") {
+        b.style.display = "block";
+    } else {
+        b.style.display = "none";
+    }
+}
