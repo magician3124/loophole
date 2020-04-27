2b85482be6e33ac861b28a5064fb72b1c321e48f
magician3124 <crashingballoon@gmail.com>
2020-04-27 16:42:29 +0800

    add displayjs for data

--- display.js
+++ display.js
@@ -1,29 +1,29 @@
 function appearphysic() {
     var a = document.getElementById("physic");
     var ablock = document.getElementById("physicalblock");
-    var text = document.getElementById("physicaltext");
+    var atext = document.getElementById("physicaltext");
     if (a.style.display === "none") {
         a.style.display = "block";
         ablock.style.backgroundColor = "orange";
-        text.style.color = "white";
+        atext.style.color = "white";
     } else {
         a.style.display = "none";
         ablock.style.backgroundColor = "white";
-        text.style.color = "#9F92CA";
+        atext.style.color = "#9F92CA";
     }
 }
-// 
-// function appeardata() {
-//     var a = document.getElementById("data");
-//     var ablock = document.getElementById("datablock");
-//     var text = document.getElementById("datatext");
-//     if (a.style.display === "none") {
-//         a.style.display = "block";
-//         ablock.style.backgroundColor = "orange";
-//         text.style.color = "white";
-//     } else {
-//         a.style.display = "none";
-//         ablock.style.backgroundColor = "white";
-//         text.style.color = "#9F92CA";
-//     }
-// }
+
+function appeardata() {
+    var b = document.getElementById("data");
+    var bblock = document.getElementById("datablock");
+    var btext = document.getElementById("datatext");
+    if (b.style.display === "none") {
+        b.style.display = "block";
+        bblock.style.backgroundColor = "orange";
+        btext.style.color = "white";
+    } else {
+        b.style.display = "none";
+        bblock.style.backgroundColor = "white";
+        btext.style.color = "#9F92CA";
+    }
+}
