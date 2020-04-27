bf389167099a845e9b061f480e7bde0c248c61fb
magician3124 <crashingballoon@gmail.com>
2020-04-27 12:50:25 +0800

    change js

--- display.js
+++ display.js
@@ -12,18 +12,18 @@ function appearphysic() {
         text.style.color = "#9F92CA";
     }
 }
-
-function appearphysic() {
-    var a = document.getElementById("data");
-    var ablock = document.getElementById("datablock");
-    var text = document.getElementById("datatext");
-    if (a.style.display === "none") {
-        a.style.display = "block";
-        ablock.style.backgroundColor = "orange";
-        text.style.color = "white";
-    } else {
-        a.style.display = "none";
-        ablock.style.backgroundColor = "white";
-        text.style.color = "#9F92CA";
-    }
-}
+// 
+// function appeardata() {
+//     var a = document.getElementById("data");
+//     var ablock = document.getElementById("datablock");
+//     var text = document.getElementById("datatext");
+//     if (a.style.display === "none") {
+//         a.style.display = "block";
+//         ablock.style.backgroundColor = "orange";
+//         text.style.color = "white";
+//     } else {
+//         a.style.display = "none";
+//         ablock.style.backgroundColor = "white";
+//         text.style.color = "#9F92CA";
+//     }
+// }
