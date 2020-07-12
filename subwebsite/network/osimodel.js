8c24ed6a8a92b1ac942f1b82b9d09abc0483a642
magician3124 <crashingballoon@gmail.com>
2020-07-12 15:05:48 +0800

    add reference (not finish)

--- subwebsite/network/osimodel.js
+++ subwebsite/network/osimodel.js
@@ -1,6 +1,21 @@
 function physicsLayer(){
     document.getElementById("physicsLayer").scrollIntoView();
 }
+
 function signals(){
     document.getElementById("signals").scrollIntoView();
 }
+
+function transform(){
+    document.getElementById("transform").scrollIntoView();
+}
+
+function cite_1(){
+    document.getElementById("1").scrollIntoView();
+}
+
+function cite_2(){
+    document.getElementById("2").scrollIntoView();
+}
+
+
