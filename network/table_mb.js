91abeed72996459f2ac83c14c5a6a64557203fa0
magician3124 <crashingballoon@gmail.com>
2020-07-31 09:14:48 +0800

    minor change

---
+++ network/table_mb.js
@@ -0,0 +1,71 @@
+function PhysicsLayerScroll(){
+        window.scrollTo(0, document.getElementById('PhysicsLayer').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function TransformScroll(){
+        window.scrollTo(0, document.getElementById('Transform').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function Signals(){
+        window.scrollTo(0, document.getElementById('signals').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function digitalSignal(){
+        window.scrollTo(0, document.getElementById('digitalSignal').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function analogSignal(){
+        window.scrollTo(0, document.getElementById('analogSignal').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function channel(){
+        window.scrollTo(0, document.getElementById('channel').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function sync(){
+        window.scrollTo(0, document.getElementById('sync').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function config(){
+        window.scrollTo(0, document.getElementById('config').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function multi(){
+        window.scrollTo(0, document.getElementById('multi').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function fdm(){
+        window.scrollTo(0, document.getElementById('fdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function tdm(){
+        window.scrollTo(0, document.getElementById('tdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function syncTdm(){
+        window.scrollTo(0, document.getElementById('syncTdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function asyncTdm(){
+        window.scrollTo(0, document.getElementById('asyncTdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function wdm(){
+        window.scrollTo(0, document.getElementById('wdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function cdm(){
+        window.scrollTo(0, document.getElementById('cdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function topo(){
+        window.scrollTo(0, document.getElementById('topo').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function trans(){
+        window.scrollTo(0, document.getElementById('trans').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
+
+function reference(){
+        window.scrollTo(0, document.getElementById('refer').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+}
