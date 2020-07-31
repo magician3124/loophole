cb3b981c7e3013365ae4eb3c570d18761ee9194a
magician3124 <crashingballoon@gmail.com>
2020-07-31 09:39:04 +0800

    change table_mb to 385

--- network/table_mb.js
+++ network/table_mb.js
@@ -1,75 +1,75 @@
 function PhysicsLayerScroll(){
-        window.scrollTo(0, document.getElementById('PhysicsLayer').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('PhysicsLayer').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function TransformScroll(){
-        window.scrollTo(0, document.getElementById('Transform').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('Transform').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function Signals(){
-        window.scrollTo(0, document.getElementById('signals').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('signals').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function digitalSignal(){
-        window.scrollTo(0, document.getElementById('digitalSignal').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('digitalSignal').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function analogSignal(){
-        window.scrollTo(0, document.getElementById('analogSignal').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('analogSignal').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function channel(){
-        window.scrollTo(0, document.getElementById('channel').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('channel').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function sync(){
-        window.scrollTo(0, document.getElementById('sync').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('sync').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function config(){
-        window.scrollTo(0, document.getElementById('config').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('config').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function multi(){
-        window.scrollTo(0, document.getElementById('multi').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('multi').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function fdm(){
-        window.scrollTo(0, document.getElementById('fdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('fdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function tdm(){
-        window.scrollTo(0, document.getElementById('tdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('tdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function syncTdm(){
-        window.scrollTo(0, document.getElementById('syncTdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('syncTdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function asyncTdm(){
-        window.scrollTo(0, document.getElementById('asyncTdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+        window.scrollTo(0, document.getElementById('asyncTdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function wdm(){
-        window.scrollTo(0, document.getElementById('wdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+        window.scrollTo(0, document.getElementById('wdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function cdm(){
-        window.scrollTo(0, document.getElementById('cdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('cdm').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function topo(){
-        window.scrollTo(0, document.getElementById('topo').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('topo').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function trans(){
-        window.scrollTo(0, document.getElementById('trans').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('trans').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function reference(){
-        window.scrollTo(0, document.getElementById('refer').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('refer').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
 
 function cite(){
-        window.scrollTo(0, document.getElementById('refer').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-250);
+        window.scrollTo(0, document.getElementById('refer').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-385);
 }
