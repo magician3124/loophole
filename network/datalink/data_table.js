fbbe90ab8b1cfa517bdddfbe50267ce85d7fed40
magician3124 <crashingballoon@gmail.com>
2020-08-14 08:24:43 +0800

    update table of content

---
+++ network/datalink/data_table.js
@@ -0,0 +1,59 @@
+function DataScroll(){
+        window.scrollTo(0, document.getElementById('DataScroll').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function HubScroll(){
+        window.scrollTo(0, document.getElementById('HubScroll').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function SwitchScroll(){
+        window.scrollTo(0, document.getElementById('SwitchScroll').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function MacScroll(){
+        window.scrollTo(0, document.getElementById('MacScroll').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function BridgeScroll(){
+        window.scrollTo(0, document.getElementById('BridgeScroll').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function RouterScroll(){
+        window.scrollTo(0, document.getElementById('RouterScroll').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function EthernetScroll(){
+        window.scrollTo(0, document.getElementById('EthernetScroll').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function BroadcastScroll(){
+        window.scrollTo(0, document.getElementById('BroadcastScroll').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function FramingScroll(){
+        window.scrollTo(0, document.getElementById('FramingScroll').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function FixedScroll(){
+        window.scrollTo(0, document.getElementById('FixedScroll').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function LengthScroll(){
+        window.scrollTo(0, document.getElementById('LengthScroll').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function FlagsScroll(){
+        window.scrollTo(0, document.getElementById('FlagsScroll').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function StuffingScroll(){
+        window.scrollTo(0, document.getElementById('StuffingScroll').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function ConsistentScroll(){
+        window.scrollTo(0, document.getElementById('ConsistentScroll').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
+
+function cite(){
+        window.scrollTo(0, document.getElementById('refer').offsetTop - (document.getElementsByClassName('FixedElement')[0].offsetHeight)-150);
+}
